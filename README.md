0a07b41a9062c0b209e41a65522389d2e82ceb42
Yuji Iwai <iwai@sonicmoov.com>
2015-02-04 18:24:21 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # model-metadata-redis-for-phalcon
 Adapt to Redis the model metadata of Phalcon
+
+# Usage
+
+```php
+
+use Phalcon\Mvc\Application;
+use Iwai\Phalcon\Mvc\Model\MetaData\Redis as MetaDataRedis;
+
+$app = new Application();
+
+$app->getDI()->setShared('modelsMetadata', function () {
+    return new MetaDataRedis([
+        'host' => '127.0.0.1',
+        'port' => 6379,
+    ]);
+});
+
+```
+
+# Options
+
+## host
+  Redis server host
+## port
+  Redis server port
+## prefix
+  Set hset prefix key for Redis (default `__metadata`)
+## lifetime
+  Set timeout for cache (default 3600 sec)
---
+++ src/Redis.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Redis.php
+ *
+ * @version     $Id$
+ *
+ */
+
+
+namespace Iwai\Phalcon\Mvc\Model\MetaData;
+
+use Redis;
+use Phalcon\Mvc\Model\MetaData;
+
+class Redis extends MetaData {
+
+    const DEFAULT_TTL = 3600;
+
+    private $_options = [ ];
+    private $_cache   = [ ];
+    private $_client  = null;
+
+    public function __construct($options = array())
+    {
+        $this->_options = $options;
+
+        if (!isset($this->_options['prefix']))
+            $this->_options['prefix'] = '__metadata';
+        if (!isset($this->_options['lifetime']))
+            $this->_options['lifetime'] = self::DEFAULT_TTL;
+
+        $this->_metaData = [];
+    }
+
+    private function getClient()
+    {
+        if (!isset($this->_client)) {
+            $client = new \Redis();
+            $client->pconnect(
+                $this->_options['host'], $this->_options['port'], 0, gethostname()
+            );
+            $client->setOption(\Redis::OPT_SERIALIZER, \Redis::SERIALIZER_PHP);
+            $client->select(0);
+
+            $this->_client = $client;
+        }
+        return $this->_client;
+    }
+
+    public function read($key)
+    {
+        if (isset($this->_cache[ $key ]))
+            return $this->_cache[ $key ];
+
+        try {
+            if (false === ($value = $this->getClient()->hGet($this->_options['prefix'], $key)))
+                return null;
+            return $value;
+        } catch (\Exception $e) {
+            return null;
+        }
+    }
+
+    public function write($key, $data)
+    {
+        $this->_cache[ $key ] = $data;
+
+        try {
+            $this->getClient()->hSet($this->_options['prefix'], $key, $data);
+
+            if ($this->_options['lifetime'] > 0)
+                $this->getClient()->setTimeout(
+                    $this->_options['prefix'], $this->_options['lifetime']
+                );
+        } catch (\Exception $e) {
+
+        }
+    }
+
+    public function reset()
+    {
+        try {
+            $keys = $this->getClient()->hGetAll($this->_options['prefix']);
+
+            if (is_array($keys)) {
+                foreach ($keys as $k) {
+                    $this->getClient()->hDel($this->_options['prefix'], $k);
+                }
+            }
+        } catch (\Exception $e) {
+
+        }
+        parent::reset();
+    }
+
+}
